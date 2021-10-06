652444488f6f4502a3dcaaf23089a629ec7f2ec5
rahulrajnataraj <38761978+rahul162raj@users.noreply.github.com>
2021-10-06 20:00:52 +0530

    Unwanted dir removed

--- Common Programs/ChangeTimestamp.js
+++
@@ -1,9 +0,0 @@
-// Vid #4 Ques #3 Interviewnest mock interview
-
-
-let time = "09:03PM"
-
-if(time.endsWith("PM")){
-    let hour = Number(time.slice(0,2)) + 12;
-    console.log(hour + time.slice(2,5));
-}
--- Common Programs/RecursiveReverse.js
+++
@@ -1,9 +0,0 @@
-function reverseWord(index, str){
-    if(index == str.length) return index;
-    else{
-        reverseWord(index+1, str);
-        console.log(str[index]);
-    }
-}
-
-reverseWord(0, "Rahul Raj");
